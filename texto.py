99627177f6bd14b05e977d91a76985f976e7a61d
crozasc <72705196+crozasc@users.noreply.github.com>
2022-04-04 11:13:48 -0400

    Update texto.py

--- texto.py
+++ texto.py
@@ -1,38 +1,36 @@
-
 class texto:
     def __init__(self, nombre):
-        contenido = open(nombre, "r")
-        lineas = 0
-        palabras = 0
-        palabrasNoRepetidas = 0
-        caracteresConEspacio = 0
-        caracteresSinEspacio = 0
+        self.contenido = open(nombre, "r")
+        self.lineas = 0
+        self.palabras = 0
+        self.palabrasUnicas = 0
+        self.caracteresConEspacio = 1
+        self.caracteresSinEspacio = 1
+        
 
     def estadisticasTexto(self):
         for i in self.contenido:
             self.contarCaracteresConEspacio(i)
             self.contarCaracteresSinEspacio(i)
             self.contarPalabras(i)
+            self.contarLineas(i)
 
             
     def contarCaracteresConEspacio(self, i):
-        if(i!=""): self.caracteresConEspacio+= 1
+        for j in i:
+            if(j!=""): self.caracteresConEspacio+= 1
+        self.caracteresConEspacio=self.caracteresConEspacio-1
 
     def contarCaracteresSinEspacio(self, i):
-        if(i!=" "): 
-            self.caracteresConEspacio+= 1
-        elif (i==" "):
-            self.caracteresConEspacio-= 1
+        for j in i:
+            if(j!=" "): 
+                self.caracteresSinEspacio+= 1
+        self.caracteresSinEspacio=self.caracteresSinEspacio-1
     
     def contarPalabras(self, i):
-        if(i==" "):
-            self.palabras+=1
+        for j in i:
+            if(j==" "):self.palabras+=1
+        self.palabras=self.palabras+1
     
     def contarLineas(self, i):
-        if(i=="\n"):
-            self.palabras+=1
-
-
-
-
-
+        self.lineas+=1
